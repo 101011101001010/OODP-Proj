d5d75b31969137404a8f41df75bcfab58c3536dd
Enrongkoh <48940083+Enrongkoh@users.noreply.github.com>
2019-03-26 21:50:59 +0800

    tableManager

--- RestaurantMenuApp/src/Classes/Table.java
+++ RestaurantMenuApp/src/Classes/Table.java
@@ -1,15 +1,36 @@
 package Classes;
 
 public class Table {
-	String tableID;
-	int pax;
-	boolean occupied;
-	int orderID;
-	public Table(int pax,String tableID)
+	private String tableID;
+	private int pax;
+	private int occupied;
+	private int orderID;
+
+	public Table(String tableID)
 	{
-		this.occupied = false;
+		this.occupied = 0;
 		this.orderID = -1;
-		this.pax = pax;
+		this.pax = 0;
 		this.tableID = tableID;
+	}
+
+	public String getTableID() {
+		return tableID;
+	}
+
+	public void setOccupied(int occupied){
+		this.occupied = occupied;
+	}
+
+	public int isOccupied() {
+		return occupied;
+	}
+
+	public void setOrderID(int orderID){
+		this.orderID = orderID;
+	}
+
+	public void setPax(int pax){
+		this.pax = pax;
 	}
 }
