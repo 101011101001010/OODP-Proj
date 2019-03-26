c811e9c17bf7a3f7227c71ac642e0abe43f17096
Teo Wei Jie <wjayteo@gmail.com>
2019-03-26 16:24:32 +0800

    FileIOHandler and supporting constants

---
+++ RestaurantMenuApp/src/constants/AppConstants.java
@@ -0,0 +1,37 @@
+package constants;
+
+public class AppConstants {
+
+    public static final int MENU_ID = 0;
+    public static final int PROMO_ID = 100;
+    public static final int IO_ID = 1000;
+
+    public static final String FILE_DIR = "C:/RRPSS/";
+
+    public static final String FILE_PROMO= "promo";
+    public static final String FILE_STAFF = "staff";
+
+    public static final String[] CODE_NAMES = {
+            "items.FoodMenu",
+            "items.Promo",
+            "orders.Order",
+            "tables.Booking",
+            "staff.Staff",
+            "sales.Sales"
+    };
+
+    public static final String[] FILE_NAMES = {
+        "starter",
+        "main",
+        "side",
+        "dessert",
+        "drink",
+        FILE_PROMO,
+        "order",
+        "booking",
+        "tables",
+        "revenue",
+        FILE_STAFF
+    };
+
+}
