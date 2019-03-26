9c8eabb36c3bbeae8fe359b75116e305866f596f
Isaac Yang <isaacylr96@hotmail.com>
2019-03-26 21:49:28 +0800

    Order update

--- RestaurantMenuApp/src/Classes/Order.java
+++ RestaurantMenuApp/src/Classes/Order.java
@@ -3,6 +3,8 @@ package Classes;
 import java.sql.Date;
 import java.util.ArrayList;
 
+import constants.AppConstants;
+import tools.FileIOHandler;
 import tools.MenuFactory;
 
 public class Order {
@@ -48,14 +50,21 @@ public class Order {
 	}
 	public void print(String tableID)
 	{
+		float totalPrice = 0;
+		String writeData=tableID + ", " + orderID +", ";
 		String title = "Bill for "+tableID;
 		String[] items = new String[this.orderItemList.size()];
 		for(int i =0; i<orderItemList.size();i++)
 		{
+			writeData+=orderItemList.get(i).itemID + ", " + orderItemList.get(i).count + ", " + orderItemList.get(i).price+", ";
+			totalPrice+=orderItemList.get(i).price;
 			orderItemList.get(i).addCount();
 			items[i] = orderItemList.get(i).print();
 		}
+		writeData+=totalPrice+'/';
+		items[orderItemList.size()+1] = "Total: "+totalPrice;
 		MenuFactory.printMenu(title, items);
+		FileIOHandler.write(AppConstants.FILE_NAMES[5].toLowerCase(), writeData);
 		//saveItemToRevenue
 	}
 }
