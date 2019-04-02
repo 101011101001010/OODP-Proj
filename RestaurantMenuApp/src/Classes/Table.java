d2d69a11b4611ad2c3cb5adefe6867dc127d584d
Enrongkoh <48940083+Enrongkoh@users.noreply.github.com>
2019-04-02 15:35:57 +0800

    Table manager update

--- RestaurantMenuApp/src/Classes/Table.java
+++ RestaurantMenuApp/src/Classes/Table.java
@@ -1,24 +1,51 @@
 package Classes;
 
-public class Table {
-	public String tableID;
-	public int pax;
-	public int occupied;
-	public int orderID;
+import java.util.ArrayList;
+import java.util.Date;
 
-	public Table(String tableID)
+
+public class Table {
+	private int tableID;
+	private int pax;
+	private int occupied;
+	private int orderID;
+	private ArrayList<Reservations> reservationList = new ArrayList<>();
+
+	static class Reservations{
+		private String contact;
+		private String name;
+		private String date;
+		private int pax;
+
+
+		public Reservations (String contact,String name, String date, int pax){
+			this.contact = contact;
+			this.name = name;
+			this.date = date;
+			this.pax = pax;
+		}
+		public String getDate(){
+			return date;
+		}
+	}
+
+	public Table(int tableID)
 	{
-		this.occupied = 0;
+		this.occupied = 1;
 		this.orderID = -1;
 		this.pax = 0;
 		this.tableID = tableID;
+
 	}
 
-	public String getTableID() {
+	public int getTableID() {
 		return tableID;
 	}
+	public int getOrderID() {
+		return orderID;
+	}
 
-	public void setOccupied(int occupied){
+	public  void setOccupied(int occupied){
 		this.occupied = occupied;
 	}
 
@@ -33,4 +60,26 @@ public class Table {
 	public void setPax(int pax){
 		this.pax = pax;
 	}
+
+	public ArrayList<Reservations> getReservationList(){
+		return reservationList;
+	}
+
+	public String toString(){
+		String check=null;
+		if (isOccupied()==-1)
+			check = "Reserved.";
+		if (isOccupied()==0)
+			check = "not occupied.";
+		if (isOccupied()==1)
+			check = "occupied.";
+		return "Table " + getTableID() + " is " + check;
+	}
+
+
+	public void isReserved(String contact,String name, String date, int pax){
+		reservationList.add(new Reservations(contact, name, date, pax));
+	}
+
 }
+
--- RestaurantMenuApp/src/Classes/TableManager.java
+++ RestaurantMenuApp/src/Classes/TableManager.java
@@ -1,149 +1,164 @@
 package Classes;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 public class TableManager {
 
-	public static ArrayList<Table> tableList = new ArrayList<Table>();
-	private static Map<String, Integer> map;
-	static boolean done = false;
+	public static ArrayList<Table> tableList = new ArrayList<>();
+	private static Map<Integer, Integer> map;
 
-	static
-	{
+	public TableManager() {
 		map = new HashMap<>();
 
-		String id;
+		int id = 20;
 		int count = 0;
+		for (int i = 0; i < 30; i++) {
 
-		for (int i = 0; i<10; i++)
-		{
-			id = "2" + i;
 			tableList.add(new Table(id));
-			map.put(id, count);
-			count++;
-		}
-		for (int i = 0; i<10; i++)
-		{
-			id = "4" + i;
-			tableList.add(new Table(id));
-			map.put(id, count);
-			count++;
-		}
-		for (int i = 0; i<5; i++)
-		{
-			id = "8" + i;
-			tableList.add(new Table(id));
-			map.put(id, count);
-			count++;
-		}
-		for (int i = 0; i<5; i++)
-		{
-			id = "10" + i;
-			tableList.add(new Table(id));
-			map.put(id, count);
-			count++;
-		}
-
-		done = true;
-	}
-
-	public static void choices(Scanner s)
-	{
-		int choice = -1;
-		while (choice != 0){
-			System.out.println("1. Show Table");
-			System.out.println("1. Set Table");
-			System.out.println("1. Show Table");
+			map.put(id++, count++);
+			if (id == 30)
+				id = 40;
+			if (id == 50)
+				id = 80;
+			if (id == 85)
+				id = 100;
 		}
 	}
 
-	public static void checkVacancy()
-	{
-		int index = 0;
-		System.out.println("Tables with 2 seats.");
-		for (; index < 10; index++) {
-			System.out.println("Table " + tableList.get(index).getTableID() + " is ");
-			switch (tableList.get(index).isOccupied()) {
-				case -1:
-					System.out.print("reserved.");
-					break;
-				case 0:
-					System.out.print("occupied.");
-					break;
-				case 1:
-					System.out.print("not occupied.");
-					break;
-			}
-		}
-
-		System.out.println("Tables with 4 seats.");
-		for (; index < 20; index++) {
-			System.out.println("Table " + tableList.get(index).getTableID() + " is ");
-			switch (tableList.get(index).isOccupied()) {
-				case -1:
-					System.out.print("reserved.");
-					break;
-				case 0:
-					System.out.print("occupied.");
-					break;
-				case 1:
-					System.out.print("not occupied.");
-					break;
-			}
-		}
-		System.out.println("Tables with 8 seats.");
-		for (; index < 25; index++) {
-			System.out.println("Table " + tableList.get(index).getTableID() + " is ");
-			switch (tableList.get(index).isOccupied()) {
-				case -1:
-					System.out.print("reserved.");
-					break;
-				case 0:
-					System.out.print("occupied.");
-					break;
-				case 1:
-					System.out.print("not occupied.");
-					break;
-			}
-		}
-		System.out.println("Tables with 10 seats.");
-		for (; index < 30; index++) {
-			System.out.println("Table " + tableList.get(index).getTableID() + " is ");
-			switch (tableList.get(index).isOccupied()) {
-				case -1:
-					System.out.print("reserved.");
-					break;
-				case 0:
-					System.out.print("occupied.");
-					break;
-				case 1:
-					System.out.print("not occupied.");
-					break;
-			}
-		}
-	}
-
-
-	public static void setOccupied(String tableID, int orderID)
-	{
+	public static void setOccupied(String tableID, int orderID) {
 		int index = map.get(tableID);
 		tableList.get(index).setOrderID(orderID);
 		tableList.get(index).setOccupied(1);
+
 	}
 
-	public static void setReserved(String tableID, int pax){
+	public static void setReserved(String tableID, int pax) {
 		int index = map.get(tableID);
 		tableList.get(index).setOccupied(-1);
 		tableList.get(index).setPax(pax);
 	}
 
-	public static void clear (String tableID)
-	{
+	public static void clear(String tableID) {
 		int index = map.get(tableID);
 		tableList.get(index).setOccupied(0);
+		tableList.get(index).setOrderID(-1);
+	}
+
+	public void choices(Scanner s) {
+		System.out.println("1. Show Table");
+		System.out.println("1. Set Table");
+		System.out.println("1. Set reservation");
+		System.out.println("");
+
+	}
+
+	public void checkVacancy() {
+		for (Table t : tableList) {
+			System.out.println(t.toString());
+		}
+	}
+
+	public int getOrderID(String tableID){
+		return tableList.get(map.get(tableID)).getOrderID();
+	}
+
+	//
+	//
+	//
+
+	public void addReservation(Scanner s) {
+		String date, name, contact;
+		int pax, index;
+		System.out.println("Enter reserving date(ddmmyyyy) : ");
+		date = s.next();
+		System.out.println("Enter reserving time(hh:mm(am/pm))");
+		date = date + s.next();
+
+		System.out.println(format(date).toLocalTime());
+
+		do {
+			System.out.println("Enter number of people.");
+			pax = s.nextInt();
+			if (pax > 10) {
+				System.out.println("Tables are limited to a maximum number of 10 people, please enter again.");
+			}
+			if (pax < 1) {
+				System.out.println("Unable to book a table with less than a person, please enter again.");
+			}
+		} while (pax > 10 || pax < 1);
+
+		/*index = checkReservation(date, pax);
+
+		if(index != -1){
+			System.out.println("Enter Your Name.");
+			name = s.next();
+			System.out.println("Enter Your Contact.");
+			contact = s.next();
+			tableList.get(index).getReservationList().add(new Table.Reservations(contact, name, date, pax));
+		}*/
+	}
+
+	public int checkReservation(String datetime, int pax) {
+		int index;
+		int noOfTables = 0;
+		DateTimeFormatter session = DateTimeFormatter.ofPattern("a");
+
+		for (Table t : tableList) {
+			if (pax <= 2 && (t.getTableID() / 10 == 2)) {
+				for (Table.Reservations r : t.getReservationList()) {
+					if ((format(datetime).toLocalDate().compareTo(format(r.getDate()).toLocalDate()) != 0)
+							&& (format(datetime).format(session) == format(r.getDate()).format(session))) {
+						noOfTables = noOfTables+1;
+						if (noOfTables>=10)
+							return -1;
+					}
+				}
+			}
+			if ((pax == 3 || pax == 4) && (t.getTableID() / 10 == 4)) {
+				for (Table.Reservations r : t.getReservationList()) {
+					if ((format(datetime).toLocalDate().compareTo(format(r.getDate()).toLocalDate()) == 0)
+							&& (format(datetime).format(session) == format(r.getDate()).format(session))) {
+						noOfTables = noOfTables+1;
+						if (noOfTables>=10)
+							return -1;
+					}
+				}
+			}
+			if (pax>=5 && pax<=8 && (t.getTableID() / 10 == 8)) {
+				for (Table.Reservations r : t.getReservationList()) {
+					if ((format(datetime).toLocalDate().compareTo(format(r.getDate()).toLocalDate()) == 0)
+							&& (format(datetime).format(session) == format(r.getDate()).format(session))) {
+						noOfTables = noOfTables+1;
+						if (noOfTables>=10)
+							return -1;
+					}
+				}
+			}
+			if (pax>=9 && (t.getTableID() / 10 == 10)) {
+				for (Table.Reservations r : t.getReservationList()) {
+					if ((format(datetime).toLocalDate().compareTo(format(r.getDate()).toLocalDate()) == 0)
+							&& (format(datetime).format(session) == format(r.getDate()).format(session))) {
+						noOfTables = noOfTables+1;
+						if (noOfTables>=10)
+							return -1;
+					}
+				}
+			}
+		}
+
+
+		//index = map.get(tableID);
+		return 0;
+
+
+	}
+	public LocalDateTime format (String datetime) {
+
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("ddMMyyyyhh:mma");
+		return LocalDateTime.parse(datetime, formatter);
 	}
 }
+
 
