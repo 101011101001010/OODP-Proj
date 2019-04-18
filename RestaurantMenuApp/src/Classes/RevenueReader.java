f809e8b4f9b30d6889d7774b25bba0126658ed0a
Enrongkoh <48940083+Enrongkoh@users.noreply.github.com>
2019-04-18 17:51:24 +0800

    Revenue update

--- RestaurantMenuApp/src/Classes/RevenueReader.java
+++ RestaurantMenuApp/src/Classes/RevenueReader.java
@@ -5,13 +5,11 @@ import core.RestaurantManager;
 import enums.DataType;
 import tools.ConsolePrinter;
 import tools.FileIO;
-
 import java.lang.reflect.Method;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
-import java.util.stream.Collectors;
 
 
 public class RevenueReader extends RestaurantManager {
@@ -23,7 +21,6 @@ public class RevenueReader extends RestaurantManager {
     }
 
     private List<String> stringList;
-    private List<LocalDateTime> dateList;
     private DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyyMMdd HHmmss");
     private Map<String, Integer>  totalCount;
     private Map<String, BigDecimal> totalPrice;
@@ -66,7 +63,8 @@ public class RevenueReader extends RestaurantManager {
             Method method = this.getClass().getDeclaredMethod("print" + suffix);
             method.invoke(this);
         } catch (Exception e) {
-            System.out.println("No method found." + e.getMessage());
+            ConsolePrinter.logToFile("Method error", e);
+            //System.out.println("No method found." + e.getMessage());
         }
     }
 
@@ -75,7 +73,6 @@ public class RevenueReader extends RestaurantManager {
         LocalDateTime checkdate;
         this.totalCount = new HashMap<>();
         this.totalPrice = new HashMap<>();
-
 
         for ( String s1 : stringList) {
             String[] split = s1.split(" // ");
@@ -90,84 +87,123 @@ public class RevenueReader extends RestaurantManager {
                     String orderName = itemDetails[0];
                     int count = Integer.parseInt(itemDetails[1]);
                     BigDecimal price = new BigDecimal(itemDetails[2]);
+                    addList(todayList, orderName);
+                    increaseCount(totalCount, orderName, count);
+                    addPrice(totalPrice, orderName, price);
                 }
             }
         }
-
-
-
-            // ordername - count - price--ordername - count - price
-            //if (LocalDateTime.parse(str.split(" // ")[0]).toLocalDate().equals(LocalDateTime.now())){
-
-            //}
-        /*
-        stringList.stream().filter(str -> LocalDateTime.parse(str.split(" // ")[0], format).toLocalDate().equals(LocalDateTime.now().toLocalDate())).forEach(todayList::add);
-        todayList = todayList.stream().map(str -> str.replace("--", "\n")).collect(Collectors.toList());
-        ConsolePrinter.printTable("Today's Revenue", "Order ID // Staff ID // Total // Individual Items", todayList, true);
-        getInputHelper().getInt("", 0, 0);*/
+        printRevenue(totalCount, totalPrice, todayList);
     }
 
+    private void printMonthly() {
+        List<String> monthlyList = new ArrayList<>();
+        LocalDateTime checkdate;
+        this.totalCount = new HashMap<>();
+        this.totalPrice = new HashMap<>();
 
+        for ( String s1 : stringList) {
+            String[] split = s1.split(" // ");
+            String orderId = split[0];
+            checkdate = LocalDateTime.parse(orderId, format);
+            if(checkdate.getMonth().equals(LocalDateTime.now().getMonth()) &&
+                    checkdate.getYear() == LocalDateTime.now().getYear()){
+                String items = split[1];
+                String[] itemArray = items.split("--");
 
-    public void loadData() {
-        /*
-        List<String> fileData = FileIO.read(AppConstants.FILE_NAMES[5].toLowerCase());
-        String[] lineData;
-        if (fileData != null) {
-            revenueList = new ArrayList<>();
-            itemTypes = new HashSet<>();
-            cId = new AtomicInteger(-1);
-
-            for (String line : fileData) {
-                lineData = line.split(AppConstants.FILE_SEPARATOR);
-
-                if (lineData.length == 4) {
-                    RevenueReader item = new RevenueReader(Integer.parseInt(lineData[0]), Integer.parseInt(lineData[1]), Integer.parseInt(lineData[2]), Float.parseFloat(lineData[3]));
-                    revenueList.add(item);
+                for (String s2 : itemArray) {
+                    String[] itemDetails = s2.split(" - ");
+                    String orderName = itemDetails[0];
+                    int count = Integer.parseInt(itemDetails[1]);
+                    BigDecimal price = new BigDecimal(itemDetails[2]);
+                    addList(monthlyList, orderName);
+                    increaseCount(totalCount, orderName, count);
+                    addPrice(totalPrice, orderName, price);
                 }
             }
         }
-        */
+        printRevenue(totalCount, totalPrice, monthlyList);
     }
 
-    /*
-    public void printAll() {
-        for (int i = 0; i < revenueList.size(); i++) {
-            System.out.println(revenueList.get(i).toString());
-        }
-    }
+    private void printAnnually() {
+        List<String> annuallyList = new ArrayList<>();
+        LocalDateTime checkdate;
+        this.totalCount = new HashMap<>();
+        this.totalPrice = new HashMap<>();
 
-    public void itemTotal() {
-        int temp = -1;
-        ArrayList<RevenueReader> totalList = new ArrayList<RevenueReader>();
+        for ( String s1 : stringList) {
+            String[] split = s1.split(" // ");
+            String orderId = split[0];
+            checkdate = LocalDateTime.parse(orderId, format);
+            if(checkdate.getYear() == LocalDateTime.now().getYear()){
+                String items = split[1];
+                String[] itemArray = items.split("--");
 
-        for (int i = 0; i < revenueList.size(); i++) {
-            temp = searchList(revenueList.get(i).itemID, totalList);
-            if (temp == -1) {
-                totalList.add(revenueList.get(i));
-            } else {
-                totalList.get(temp).itemCount += revenueList.get(i).itemCount;
-                totalList.get(temp).totalPrice += revenueList.get(i).totalPrice;
+                for (String s2 : itemArray) {
+                    String[] itemDetails = s2.split(" - ");
+                    String orderName = itemDetails[0];
+                    int count = Integer.parseInt(itemDetails[1]);
+                    BigDecimal price = new BigDecimal(itemDetails[2]);
+                    addList(annuallyList, orderName);
+                    increaseCount(totalCount, orderName, count);
+                    addPrice(totalPrice, orderName, price);
+                }
             }
         }
-        print(totalList);
+        printRevenue(totalCount, totalPrice, annuallyList);
     }
 
-    public int searchList(int id, ArrayList<RevenueReader> list) {
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).itemID == id) {
-                return i;
+    private void printLifetime() {
+        List<String> allList = new ArrayList<>();
+        this.totalCount = new HashMap<>();
+        this.totalPrice = new HashMap<>();
+
+        for ( String s1 : stringList) {
+            String[] split = s1.split(" // ");
+            String items = split[1];
+            String[] itemArray = items.split("--");
+
+            for (String s2 : itemArray) {
+                String[] itemDetails = s2.split(" - ");
+                String orderName = itemDetails[0];
+                int count = Integer.parseInt(itemDetails[1]);
+                BigDecimal price = new BigDecimal(itemDetails[2]);
+                addList(allList, orderName);
+                increaseCount(totalCount, orderName, count);
+                addPrice(totalPrice, orderName, price);
             }
         }
-        return -1;
+        printRevenue(totalCount, totalPrice, allList);
     }
 
-    public void print(ArrayList<RevenueReader> list) {
-        for (int i = 0; i < list.size(); i++) {
-            System.out.println(list.get(i).toString());
+    private void addList (List<String> list, String itemName){
+        for (String l :list){
+            if (l.equals(itemName))
+                return;
+        }
+        list.add(itemName);
+    }
+    private void addPrice (Map<String, BigDecimal> map, String key, BigDecimal price){
+        BigDecimal itemPrice = map.get(key);
+        if (itemPrice == null){
+            map.put(key, price);
         }
     }
-    */
+    private void increaseCount(Map<String, Integer> map, String key, int count){
+
+        Integer value = map.get(key);
+        if (value == null){
+            map.put(key, count);
+        }
+        else{
+            map.put(key, value + count);
+        }
+    }
+    private void printRevenue(Map<String, Integer> totalCount, Map<String, BigDecimal> totalPrice, List<String> revenueList){
+        BigDecimal total;
+        for ( String l : revenueList){
+            total = new BigDecimal(totalCount.get(l));
+            System.out.println(totalCount.get(l) + " " + l + " is/are sold of a total of " + totalPrice.get(l).multiply(total));
+        }
+    }
 }
-
-
