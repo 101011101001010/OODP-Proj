95ed9eeab12be4818dd3473a3e7ceca52206bc84
Teo Wei Jie <wjayteo@gmail.com>
2019-04-19 22:48:04 +0800

    Fixed javadocs missing non public stuffs

--- RestaurantMenuApp/generate_docs.bat
+++ RestaurantMenuApp/generate_docs.bat
@@ -1,5 +1,5 @@
 @echo off
 title Generate Javadocs
-javadoc -sourcepath src -d ./docs core enums menu revenue staff tables tools
+javadoc -sourcepath src -d ../Javadocs -private -noqualifier all core enums menu revenue staff tables tools
 
 pause
